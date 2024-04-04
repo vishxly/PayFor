c38902d4780db37a5572405e4cc3a50c1153d70f
Vishal Yadav <vishalyxo@gmail.com>
2024-04-04 17:11:30 +0530

    Commit

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export function Image(props) {
-        
-
-        return (
-                <>
-                 <div className='w-24 h-24 shadow-2xl object-center ml-40'>
-                <img src='/Logo.jpeg' className='rounded-2xl'/>
-                </div>       
-                </>
-        )
+  return (
+    <>
+      <div className="w-24 h-24 shadow-2xl object-center ml-40">
+        <img src="./log" className="rounded-2xl" />
+      </div>
+    </>
+  );
 }
